1a677d7c6600d24b3066b91919dc93d4e75351ba
Shain Dholakiya <shainshain99@gmail.com>
2019-04-11 19:19:13 -0400

    Updates

---
+++ precache-manifest.f1332584ce27890ae3a079c1e0357432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bab5c69072c2b404d91",
+    "url": "/ShainDholakiya/ClickyGame/static/css/main.507f9288.chunk.css"
+  },
+  {
+    "revision": "6bab5c69072c2b404d91",
+    "url": "/ShainDholakiya/ClickyGame/static/js/main.5c031a73.chunk.js"
+  },
+  {
+    "revision": "6cf3f58d4dfbe7090d88",
+    "url": "/ShainDholakiya/ClickyGame/static/js/runtime~main.dae97bbd.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ShainDholakiya/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "7c63107c37c67a3eeba71758b1d6bd83",
+    "url": "/ShainDholakiya/ClickyGame/index.html"
+  }
+];
